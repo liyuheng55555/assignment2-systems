9cea69c5c9a9952ac6383a6382478f8da5265cfa
liyuheng <liyuheng55555@126.com>
2026-05-16 04:27:15 +0800

    benchmark init

--- cs336_systems/ch1/benchmarking_script.py
+++ cs336_systems/ch1/benchmarking_script.py
@@ -123,7 +123,7 @@ def benchmark(
 if __name__ == "__main__":
     result = benchmark(
         vocab_size=10000,
-        context_length=256,
+        context_length=512,
         d_model=128,
         num_layers=4,
         num_heads=4,
@@ -134,3 +134,34 @@ if __name__ == "__main__":
         mode=2
     )
     print(result)
+    hyper_parameters = {}
+    hyper_parameters['small'] = {
+        "d_model": 768,
+        "d_ff": 3072,
+        "num_layers": 12,
+        "num_heads": 12,
+    }
+    hyper_parameters['medium'] = {
+        "d_model": 1024,
+        "d_ff": 4096,
+        "num_layers": 24,
+        "num_heads": 24,
+    }
+    hyper_parameters['large'] = {
+        "d_model": 1280,
+        "d_ff": 5120,
+        "num_layers": 36,
+        "num_heads": 20,
+    }
+    hyper_parameters['xl'] = {
+        "d_model": 2560,
+        "d_ff": 10240,
+        "num_layers": 32,
+        "num_heads": 32,
+    }
+    hyper_parameters['10B'] = {
+        "d_model": 4608,
+        "d_ff": 12288,
+        "num_layers": 50,
+        "num_heads": 36,
+    }
