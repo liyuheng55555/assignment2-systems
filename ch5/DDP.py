f3c85f43c2fcc83311c6ab5e906207f1e0a042cc
liyuheng <liyuheng55555@126.com>
2026-06-03 23:40:06 +0800

    overlap async problem

--- ch5/DDP.py
+++ ch5/DDP.py
@@ -1,3 +1,6 @@
+import threading
+from time import sleep
+
 import torch.nn
 import torch.distributed as dist
 
@@ -18,19 +21,37 @@ class OverlapDDP(torch.nn.Module):
     def __init__(self, module: torch.nn.Module):
         super().__init__()
         self.module = module
+        self.handles = []
+        self.lock = threading.Lock()
+        self.grad_count = 0
         
         def all_reduce_hook(x: torch.Tensor):
-            dist.all_reduce(x.grad, async_op=False)
-            x.grad /= dist.get_world_size()
+            handle = dist.all_reduce(x.grad, async_op=True)
+            with self.lock:
+                self.handles.append(handle)
+            # x.grad /= dist.get_world_size()
         
-        for parameter in module.parameters():
+        for parameter in self.module.parameters():
             dist.broadcast(parameter, src=0)
             if parameter.requires_grad:
                 parameter.register_post_accumulate_grad_hook(hook=all_reduce_hook)
+                self.grad_count += 1
+
 
     def forward(self, *args, **kwargs):
         return self.module(*args, **kwargs)
 
+
     def finish_gradient_synchronization(self):
-        return True
+        while len(self.handles) < self.grad_count:
+            sleep(0.1)
+        for handle in self.handles:
+            handle.wait()
+        with self.lock:
+            self.handles.clear()
+        for parameter in self.module.parameters():
+            if parameter.grad is not None:
+                parameter.grad /= dist.get_world_size()
+        return
+
 
