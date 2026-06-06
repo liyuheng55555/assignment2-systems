604737f9c1c7b84c773550fa420640c1b4edd11a
li yuheng <liyuheng55555@gmail.com>
2026-06-06 22:17:19 +0800

    try to benchmark

--- ch5/try.py
+++ ch5/try.py
@@ -4,20 +4,29 @@ from time import perf_counter
 import torch
 import torch.distributed as dist
 import torch.multiprocessing as mp
+
 def setup(rank, world_size):
     os.environ["MASTER_ADDR"] = "localhost"
     os.environ["MASTER_PORT"] = "29500"
-    dist.init_process_group("gloo", rank=rank, world_size=world_size)
+    dist.init_process_group("nccl", rank=rank, world_size=world_size)
+    torch.cuda.set_device(rank)
+
 def distributed_demo(rank, world_size):
     setup(rank, world_size)
-    data = torch.rand((1024*1024*1024//torch.float32.itemsize,))
+    device = torch.device(f"cuda:{rank}")
+    data = torch.rand((5*1024*1024*1024//torch.float32.itemsize,), device=device)
     for i in range(5):
         dist.all_reduce(data, async_op=False)
     # print(f"rank {rank} data (before all-reduce): {data}")
+    torch.cuda.synchronize()
     start = perf_counter()
-    dist.all_reduce(data, async_op=False)
-    print(f"{rank=} time cost {perf_counter()-start:.4f}s")
+    for i in range(10):
+        dist.all_reduce(data, async_op=False)
+        torch.cuda.synchronize()
+    print(f"{rank=} time cost for one round {(perf_counter()-start)/10:.6f}s")
     # print(f"rank {rank} data (after all-reduce): {data}")
+    dist.destroy_process_group()
+
 if __name__ == "__main__":
-    world_size = 4
+    world_size = 2
     mp.spawn(fn=distributed_demo, args=(world_size, ), nprocs=world_size, join=True)
